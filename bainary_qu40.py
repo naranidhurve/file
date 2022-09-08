b0b85bcc914512f781543f0dcd4d7b82fd41325f
Narani Dhurve <95540634+naranidhurve@users.noreply.github.com>
2022-09-08 02:38:37 -0400

    Add files via upload

---
+++ bainary_qu40.py
@@ -0,0 +1,17 @@
+f=open("records.text", "wb")
+a=f.write()
+a=f.write()
+a=f.write()
+n=int(input("Enter the Roll Number: "))
+b=f.write()
+i=0
+sum=0
+while i<=(f):
+    x=n.b(f)
+    if x[1]==n:
+        print("name:",x[0])
+        sum=True
+        break
+if sum==False:
+    print("enter the roll number")
+      
